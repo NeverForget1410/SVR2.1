50747fb8cde2bac1327bfb2d9e6f856328b6ec91
Patryk <p-figura@wp.pl>
2024-09-21 22:33:21 +0200

    21.09.2024
    Całkowita zmiana wyglądu - dark / light mode
    Dodano biblioteke bootstrap lokalnie ✓
    Dodano skrypty z gotowca i obsługi wyglądu strony ✓
    Przeniesiono stary wygląd do folderu history: styles_old.css, scripts_old.js, _layout_old.html
    Aktualizacja nowego layoutu o działanie podstron (hiperłącza)
    Dodanie thymeleaf fragment do zarządzania z 1 miejsca przyciskiem dodawania rekordów dostępne w pliku displayListFragments.html "addOrEditButtonTemplate"
    Dodanie thymeleaf fragment do zarządzania z 1 miejsca listą rozwilajną "pageSizeDropdownTemplate" w pliku displayListFragments.html
    Dodanie thymeleaf fragment do zarządzania z 1 miejsca listą podstron "tablePaginationTemplate" w pliku displayListFragments.html
    ----------------------------------------------------------------
    Wyświeltanie miast ✓
    Wyświetlanie adresów ✓
    Dodawanie/edycja miasta ✓
    Dodawanie/edycja adresu ✓
    Dodawanie/edycja urządzenia ✓
    Lista urządzeń ✓
    Wyświetlanie szczegółów dot. urzadzenia ✓
    Wyszukiwarka na aktualnie wyświetlaniej stronie ✓
    Wyświeltanie listy producentów ✓
    Dodawanie/edycja producenta ✓
    Wyświetlanie listy modeli urządzeń ✓
    Dodawanie/edytowanie modelem urzadzenia ✓
    Wyświetlanie klas ważenia ✓
    
    ----------------------------------
    Do zrobienia:
    !!!! Zarządzenie dostępem (ROLE) !!!!
    !!!! Weryfikacja np: data-bs-target="#auth" od listy rozwijalnej!!!!
    Wyświetlanie listy klientów ×
    Dodawanie klientów ×
    Panele administstracyjne × (do utworzenia)
    Funkcjonalności:
     - exportowanie logów ×
     - exportowanie listy urządzeń dla danego sklepu ×
     -
    
    Hierarchia menu:
    Interfejs użytkownika:
       Lokalizacje
           - Miasta
           - Adresy
       Urządzenia:
           - Lista urządzeń
           - Lista producentów
           - Lista modeli
           - Lista klas ważenia
       Klienci
         - XX- do wykonania
         - XX- do wykonania
     Administracja - do wykonania

---
+++ src/main/resources/static/JS/script.js
@@ -0,0 +1,38 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+document.querySelector(".theme-toggle").addEventListener("click",() => {
+    toggleLocalStorage();
+    toggleRootClass();
+});
+
+function toggleRootClass(){
+    const current = document.documentElement.getAttribute('data-bs-theme');
+    const inverted = current == 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme',inverted);
+}
+
+function toggleLocalStorage(){
+    if(isLight()){
+        localStorage.removeItem("light");
+    }else{
+        localStorage.setItem("light","set");
+    }
+}
+
+function isLight(){
+    return localStorage.getItem("light");
+}
+
+if(isLight()){
+    toggleRootClass();
+}
+
+var alerts = document.querySelectorAll('.auto-dismiss');
+alerts.forEach(function(alert) {
+    setTimeout(function() {
+        alert.style.display = 'none';
+    }, 2500);
+});
